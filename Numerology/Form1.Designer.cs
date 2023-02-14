12027c3afd1ac97666604b0975e8661a92897714
Denys Sliusarenko <Denys.Sliusarenko@bd.com>
2023-02-14 23:35:36 +0200

    Add project files.

---
+++ Numerology/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Numerology
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chartMariage = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.bttnCalculation = new System.Windows.Forms.Button();
+            this.dtTmPckrBirthday = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.chartMariage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chartMariage
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chartMariage.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartMariage.Legends.Add(legend1);
+            this.chartMariage.Location = new System.Drawing.Point(26, 21);
+            this.chartMariage.Name = "chartMariage";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartMariage.Series.Add(series1);
+            this.chartMariage.Size = new System.Drawing.Size(985, 677);
+            this.chartMariage.TabIndex = 0;
+            this.chartMariage.Text = "chartMariage";
+            // 
+            // bttnCalculation
+            // 
+            this.bttnCalculation.Location = new System.Drawing.Point(1047, 76);
+            this.bttnCalculation.Name = "bttnCalculation";
+            this.bttnCalculation.Size = new System.Drawing.Size(126, 27);
+            this.bttnCalculation.TabIndex = 1;
+            this.bttnCalculation.Text = "Calculation";
+            this.bttnCalculation.UseVisualStyleBackColor = true;
+            this.bttnCalculation.Click += new System.EventHandler(this.bttnCalculation_Click);
+            // 
+            // dtTmPckrBirthday
+            // 
+            this.dtTmPckrBirthday.Location = new System.Drawing.Point(1047, 34);
+            this.dtTmPckrBirthday.Name = "dtTmPckrBirthday";
+            this.dtTmPckrBirthday.Size = new System.Drawing.Size(266, 22);
+            this.dtTmPckrBirthday.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1388, 710);
+            this.Controls.Add(this.dtTmPckrBirthday);
+            this.Controls.Add(this.bttnCalculation);
+            this.Controls.Add(this.chartMariage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chartMariage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartMariage;
+        private System.Windows.Forms.Button bttnCalculation;
+        private System.Windows.Forms.DateTimePicker dtTmPckrBirthday;
+    }
+}
+
---
+++ Numerology/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Numerology
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bttnCalculation_Click(object sender, EventArgs e)
+        {
+            var dayMonth = Convert.ToInt32(dtTmPckrBirthday.Value.Day.ToString() + dtTmPckrBirthday.Value.Month.ToString());
+            var calculationNumber = dayMonth * dtTmPckrBirthday.Value.Year;
+            var digits = GetDigits(calculationNumber);
+            List<int> arr = Enumerable.Range(17, 40).ToList();
+
+            this.chartMariage.Series.Clear();
+
+            Series series = this.chartMariage.Series.Add("Chart");
+            series.ChartType = SeriesChartType.Spline;
+            //  series.LineMarkerOptions.StarPointCount = 5;
+            chartMariage.ChartAreas.FirstOrDefault().AxisX.Interval = 1;
+            chartMariage.ChartAreas.FirstOrDefault().AxisY.Interval = 1;
+
+            series.MarkerStep= 1;
+            series.MarkerSize = 1;
+            for (int i = 0, j = 0; i < arr.Count; i++, j++)
+            {
+                series.Points.AddXY(arr[i], digits[j]);
+                if (j == calculationNumber.ToString().Length - 1) 
+                { 
+                    j = 0; 
+                }
+            }
+        }
+
+        public static List<int> GetDigits(int source)
+        {
+            List<int> digits = new List<int>();
+            while (source > 0)
+            {
+                var digit = source % 10;
+                source /= 10;
+                digits.Add(digit);
+            }
+            digits.Reverse();
+            return digits;
+        }
+    }
+}
